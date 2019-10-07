91c28e3c1e091647bf668fc876184c9b3f834c7f
dato <mysmilesh@gmail.com>
2019-10-07 22:27:12 +0900

    download script for the preprocessed dataset (aaai-19, nela-17)

---
+++ data/download_processed_dataset_aaai-19.sh
@@ -0,0 +1,6 @@
+# download pre-processed dataset (AAAI-19)
+
+# whole-type + para-type data (2.5 GB)
+wget http://milabfile.snu.ac.kr:16000/detecting-incongruity/data-processed-aaai-19.tar.gz
+tar xzvf data-processed-aaai-19.tar.gz
+rm data-processed-aaai-19.tar.gz
---
+++ data/download_processed_dataset_nela-17.sh
@@ -0,0 +1,6 @@
+# download pre-processed dataset (NELA-17 based)
+
+# whole-type data (253 MB)
+wget http://milabfile.snu.ac.kr:16000/detecting-incongruity/data-processed-nela-17.tar.gz
+tar xzvf data-processed-nela-17.tar.gz
+rm data-processed-nela-17.tar.gz
