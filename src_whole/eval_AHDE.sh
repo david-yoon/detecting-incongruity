2e121fdba0da3a6a0c63df0c46a101a789fe7565
dato <mysmilesh@gmail.com>
2020-05-30 22:13:20 +0900

    real world

--- src_whole/eval_AHDE.sh
+++ src_whole/eval_AHDE.sh
@@ -3,5 +3,12 @@
 
 
 
-CUDA_VISIBLE_DEVICES=1 python eval_AHDE.py --model_path "model_ahde-TEST" --batch_size 100 --encoder_size 170 --context_size 44 --encoderR_size 22 --num_layer 1 --hidden_dim 200  --num_layer_con 1 --hidden_dim_con 100 --embed_size 300 --corpus 'news-19_whole' --data_path '../data/headline_swap_news_v2.5/whole/'
+
+# CUDA_VISIBLE_DEVICES=1 python eval_AHDE.py --model_path "model_ahde-head-v2.5-TEST" --batch_size 400 --encoder_size 170 --context_size 44 --encoderR_size 22 --num_layer 1 --hidden_dim 200  --num_layer_con 1 --hidden_dim_con 100 --embed_size 300 --corpus 'news-19_whole' --data_path '../data/headline_swap_news_v2.5/whole/'
+
+
+
+
+
+CUDA_VISIBLE_DEVICES=1 python eval_AHDE.py --model_path "model_ahde-para-v2.5-TEST" --batch_size 400 --encoder_size 170 --context_size 44 --encoderR_size 22 --num_layer 1 --hidden_dim 200  --num_layer_con 1 --hidden_dim_con 100 --embed_size 300 --corpus 'news-19_whole' --data_path '../data/paragraph_swap_news_v2.5/whole/'
 
